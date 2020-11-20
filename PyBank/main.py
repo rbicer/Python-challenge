cfb36595d538ff5e5d7e4e30061a1803c6f3833a
R Bicer <rsn.bicer@hotmail.com>
2020-11-19 21:52:58 -0500

    odev

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,7 +12,7 @@ greatest_decrease_date= []
 greatest_decrease_value = 0
 total_Profits_Losses = 0
 change_in_revenue= []
-dates=[]
+dates= []
 
 #path to collect data from Resources folder
 csvpath = os.path.join("Resources", "budget_data.csv") 
@@ -32,6 +32,7 @@ with open(csvpath, newline="") as csvfile:
         # The net total amount of "Profit/Losses" over the entire period
         total_Profits_Losses= total_Profits_Losses+int(row[1])
         
+        
         # Calculate the changes in "Profit/Losses" over the entire period, then find the average of those changes
         dates.append(row[0])
 
@@ -46,12 +47,13 @@ with open(csvpath, newline="") as csvfile:
         # The greatest increase in profits (date and amount) over the entire period
             greatest_increase_value= max(change_in_revenue)
             greatest_index_value= change_in_revenue.index(greatest_increase_value)
-            greatest_index_date= dates[greatest_index_value]
+            greatest_index_date= dates[greatest_index_value+1]
+        
 
         #  The greatest decrease in losses (date and amount) over the entire period
             greatest_decrease_value= min(change_in_revenue)
             lowest_index_value= change_in_revenue.index(greatest_decrease_value)
-            lowest_index_date= dates[lowest_index_value]
+            lowest_index_date= dates[lowest_index_value+1]
 
 
 #print outcomes 
@@ -60,9 +62,9 @@ print("________________________")
 print("Total Months: ", str(total_months))
 print("Total Profit: $", str(total_Profits_Losses))
 #print("Changes in Profit Loss: $", str(change_in_revenue)) #This is a test
-print("Average Change: $", str(sum(change_in_revenue)/len(change_in_revenue))
-print("Greatest Increase in Profits: $", str(min(change_in_revenue)))
-print("Greatest Decrease in Profits: $", str(max(change_in_revenue)))
+print("Average Change: $", str(round(sum(change_in_revenue)/len(change_in_revenue),2)))
+print("Greatest Increase in Profits: ", greatest_index_date, "$", str(max(change_in_revenue)))
+print("Greatest Decrease in Profits: ", lowest_index_date, "$", str(min(change_in_revenue)))
 
 # Text File ``
 output =open('output.txt', 'w') 
@@ -71,7 +73,7 @@ text1= "Financial Analysis"
 text2= "----------------------"
 text3= "Total Months: " + str(total_months)
 text4= "Total Profits Losses: $" + str(total_Profits_Losses)
-text5= "Average Change: $" + str(sum(change_in_revenue)/len(change_in_revenue))
+text5= "Average Change: $" + str(round(sum(change_in_revenue)/len(change_in_revenue),2))
 text6= "Greatest Increase in Profits: " + greatest_index_date +"  " + "$" + str(greatest_increase_value)
 text7= "Greatest Decrease in Profits: " +  lowest_index_date+ "  " + "$" + str(greatest_decrease_value)
 output.write("{}\n{}\n{}\n{}\n{}\n{}\n{}\n" .format(text1,text2,text3,text4,text5,text6,text7))
