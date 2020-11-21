88903875bc31368e577bca30562e6bf615ba5193
R Bicer <rsn.bicer@hotmail.com>
2020-11-21 18:47:09 -0500

    odev

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,6 +9,7 @@ TotalVotes= 0
 poll_Dic= {}
 WinnerVote= 0
 WinnerPerson=[]
+winner=[]
 
 #path to collect data from Resources folder
 csvpath = os.path.join("Resources", "election_data.csv") 
@@ -43,35 +44,34 @@ with open(csvpath, newline="") as csvfile:
        
     #The winner of the election based on popular vote
     Winnervote=max(NumberVotes)
-    
+    WinnerPerson=NumberVotes.index(Winnervote)
+    winner= Candidates[WinnerPerson]
 
-# # Print Outcomes
+#Print Outcomes
 print("ELECTION RESULTS")
 print('-'* 31)
 print("Total Votes:", str(TotalVotes))
 print('-'*31)
-
 for i in range(len(Candidates)):
     print(f"{Candidates[i]}: {PercentVotes[i]:.3f}% ({NumberVotes[i]})")
 print('-'*31)
-#print("Winner:", WinnerPerson)
+print("Winner:", winner)
 print('-'*31)
 
 #Text Files
 output =open('output.txt', 'w')
-#Lines in the text File
 text1= "ELECTION RESULTS"
 text2= "-"*31
 text3= "Total Votes:" + str(TotalVotes)
 text4= "-"*31
 output.write("{}\n{}\n{}\n{}\n".format(text1,text2,text3,text4))
 dataZipper=zip(Candidates, PercentVotes, NumberVotes)
-
 for i in range(len(Candidates)):
     text5=f"{Candidates[i]}: {PercentVotes[i]:.3f}% ({NumberVotes[i]})"
     output.write("{}\n".format(text5))
-
 text6= "-"*31
-output.write("{}\n".format(text6))
+text7="Winner:"+winner
+text8= "-"*31
+output.write("{}\n{}\n{}\n".format(text6,text7,text8))
 
 
