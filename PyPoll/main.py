c73b82cb988816acbf987419d94b0413315ce324
R Bicer <rsn.bicer@hotmail.com>
2020-11-21 18:03:02 -0500

    odev

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,6 +6,9 @@ PercentVotes= []
 Candidates= []
 NumberVotes=[]
 TotalVotes= 0
+poll_Dic= {}
+WinnerVote= 0
+WinnerPerson=[]
 
 #path to collect data from Resources folder
 csvpath = os.path.join("Resources", "election_data.csv") 
@@ -17,15 +20,58 @@ with open(csvpath, newline="") as csvfile:
        
     # Read each row of data after the header
     for row in csvreader:
-        print(f"CSV Header: {csv_header}")
-
+    
         #The total number of votes cast
         TotalVotes=TotalVotes+1
 
         #A complete list of candidates who received votes
+        if row[2] in poll_Dic.keys():
+            poll_Dic[row[2]]= poll_Dic[row[2]] +1
+
+        else: 
+            poll_Dic[row[2]]= 1  
 
         #The percentage of votes each candidate won
+    for key, value in poll_Dic.items():
+        NumberVotes.append(value)
+        Candidates.append(key)
 
-        #The total number of votes each candidate won
+    for x in NumberVotes:
+        PercentVotes.append((round((x/TotalVotes*100),3)))
 
-        #The winner of the election based on popular vote
+    dataZipper=zip(Candidates, PercentVotes, NumberVotes)
+       
+    #The winner of the election based on popular vote
+    Winnervote=max(NumberVotes)
+    
+
+# # Print Outcomes
+print("ELECTION RESULTS")
+print('-'* 31)
+print("Total Votes:", str(TotalVotes))
+print('-'*31)
+
+for i in range(len(Candidates)):
+    print(f"{Candidates[i]}: {PercentVotes[i]:.3f}% ({NumberVotes[i]})")
+print('-'*31)
+#print("Winner:", WinnerPerson)
+print('-'*31)
+
+#Text Files
+output =open('output.txt', 'w')
+#Lines in the text File
+text1= "ELECTION RESULTS"
+text2= "-"*31
+text3= "Total Votes:" + str(TotalVotes)
+text4= "-"*31
+output.write("{}\n{}\n{}\n{}\n".format(text1,text2,text3,text4))
+dataZipper=zip(Candidates, PercentVotes, NumberVotes)
+
+for i in range(len(Candidates)):
+    text5=f"{Candidates[i]}: {PercentVotes[i]:.3f}% ({NumberVotes[i]})"
+    output.write("{}\n".format(text5))
+
+text6= "-"*31
+output.write("{}\n".format(text6))
+
+
